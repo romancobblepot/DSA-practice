4c09d7823d4f0284b1028877df5b120546db0bd1
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-02 06:54:28 +0000

    Sync LeetCode submission - Jump Game (python)

---
+++ problems/jump_game/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        max_index=0
+        for i in range(len(nums)):
+            if i>max_index:
+                return False
+            max_index=max(max_index,i+nums[i])
+            if max_index>=len(nums)-1:
+                return True
+        return False
+
+
+        
