129281fdba73adf640d697cd734f8bcd2b466335
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-05 10:59:37 +0000

    Sync LeetCode submission - Copy List with Random Pointer (python)

--- problems/copy_list_with_random_pointer/solution.py
+++ problems/copy_list_with_random_pointer/solution.py
@@ -23,40 +23,24 @@ class Solution(object):
             if temp not in self.cloned:
                 new_node=Node(temp.val)
                 self.cloned[temp]=new_node
-                if not temp.next:
-                    new_node.next=None
-                elif temp.next not in self.cloned:
-                    new_temp_next=Node(temp.next.val)
-                    self.cloned[temp.next]=new_temp_next
-                    new_node.next=new_temp_next
-                else:
-                    new_node.next=self.cloned[temp.next]
-                if not temp.random:
-                    new_node.random=None                
-                elif temp.random not in self.cloned:
-                    new_random=Node(temp.random.val)
-                    self.cloned[temp.random]=new_random
-                    new_node.random=new_random
-                else:
-                    new_node.random=self.cloned[temp.random]
             else:
                 new_node=self.cloned[temp]
-                if not temp.next:
-                    new_node.next=None
-                elif temp.next not in self.cloned:
-                    new_temp_next=Node(temp.next.val)
-                    self.cloned[temp.next]=new_temp_next
-                    new_node.next=new_temp_next
-                else:
-                    new_node.next=self.cloned[temp.next]
-                if not temp.random:
-                    new_node.random=None
-                elif temp.random not in self.cloned:
-                    new_random=Node(temp.random.val)
-                    self.cloned[temp.random]=new_random
-                    new_node.random=new_random
-                else:
-                    new_node.random=self.cloned[temp.random]
+            if not temp.next:
+                new_node.next=None
+            elif temp.next not in self.cloned:
+                new_temp_next=Node(temp.next.val)
+                self.cloned[temp.next]=new_temp_next
+                new_node.next=new_temp_next
+            else:
+                new_node.next=self.cloned[temp.next]
+            if not temp.random:
+                new_node.random=None                
+            elif temp.random not in self.cloned:
+                new_random=Node(temp.random.val)
+                self.cloned[temp.random]=new_random
+                new_node.random=new_random
+            else:
+                new_node.random=self.cloned[temp.random]
             self.dummy.next=new_node
             self.dummy=new_node
             temp=temp.next
