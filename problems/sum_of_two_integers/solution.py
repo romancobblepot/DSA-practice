2e08c28296599cae4d865c5f1eb9fc34c206e93a
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-06 09:44:29 +0000

    Sync LeetCode submission - Sum of Two Integers (python)

---
+++ problems/sum_of_two_integers/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        MASK = 0xFFFFFFFF
+        MAX_INT = 0x7FFFFFFF
+        while b:
+            carry=((a&b)<<1) & MASK
+            a=(a^b)& MASK
+            b=carry
+        if a<=MAX_INT:
+            return a
+        return ~(a^MASK)
+
+
+        
