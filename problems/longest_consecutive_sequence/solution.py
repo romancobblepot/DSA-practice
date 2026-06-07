bf79bd83f8c0051e5a849982e62a36c598cc5ddb
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-07 06:26:44 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (python)

--- problems/longest_consecutive_sequence/solution.py
+++ problems/longest_consecutive_sequence/solution.py
@@ -4,16 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        numbers=set()
-        for i in range(len(nums)):
-            numbers.add(nums[i])
-        max_count=0
+        numbers=set(nums)
+        max_len=0
         for num in numbers:
             if num-1 not in numbers:
-                    start=num
-                    count=1
-                    while start+1 in numbers:
-                        count+=1
-                        start=start+1
-                    max_count=max(count,max_count)
-        return max_count
+                start=num
+                count=1
+                while start+1 in numbers:
+                    start+=1
+                    count+=1
+                max_len=max(max_len,count)
+        return max_len
+            
