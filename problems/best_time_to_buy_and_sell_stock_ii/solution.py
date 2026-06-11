8a698f550fede90ee4671f4d5728b18b4f7d9820
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-11 09:51:23 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (python)

---
+++ problems/best_time_to_buy_and_sell_stock_ii/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        n=len(prices)
+        dp=[[0]*2 for _ in range(n+1)]
+        for i in range(n-1,-1,-1):
+            dp[i][0]=max(dp[i+1][0],-prices[i]+dp[i+1][1])
+            dp[i][1]=max(dp[i+1][1],prices[i]+dp[i+1][0])
+        return dp[0][0]
+        
