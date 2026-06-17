7239eb7c10db0d63bba834b945444f5e4d444687
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-17 06:26:56 +0000

    Sync LeetCode submission - Edit Distance (python)

--- problems/edit_distance/solution.py
+++ problems/edit_distance/solution.py
@@ -5,20 +5,21 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
-        start=word1
-        target=word2
-        n=len(start)
-        m=len(target)
+        n=len(word1)
+        m=len(word2)
         dp=[[0]*(m+1) for _ in range(n+1)]
-        for i in range(n+1):
-            dp[i][0]=i
         for i in range(m+1):
             dp[0][i]=i
+        for j in range(n+1):
+            dp[j][0]=j
         for i in range(1,n+1):
             for j in range(1,m+1):
-                if start[i-1]==target[j-1]:
+                if word1[i-1]==word2[j-1]:
                     dp[i][j]=dp[i-1][j-1]
                 else:
                     dp[i][j]=1+min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
         return dp[n][m]
+
+
+
         
