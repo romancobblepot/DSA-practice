7651743a85baf62da7e16de37897150866b222c8
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-17 06:57:11 +0000

    Sync LeetCode submission - Shortest Common Supersequence  (python)

--- problems/shortest_common_supersequence_/solution.py
+++ problems/shortest_common_supersequence_/solution.py
@@ -7,40 +7,33 @@ class Solution(object):
         """
         n=len(str1)
         m=len(str2)
-        if m>n:
-            n,m=m,n
-            str1,str2=str2,str1
         dp=[[0]*(m+1) for _ in range(n+1)]
         for i in range(1,n+1):
             for j in range(1,m+1):
                 if str1[i-1]==str2[j-1]:
-                    dp[i][j]=dp[i-1][j-1]+1
+                    dp[i][j]=1+dp[i-1][j-1]
                 else:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
         i=n
         j=m
-        ans=[]
+        ans=""
         while i>0 and j>0:
             if str1[i-1]==str2[j-1]:
-                ans.append(str2[j-1])
+                ans+=str1[i-1]
                 i-=1
                 j-=1
             elif dp[i-1][j]>dp[i][j-1]:
-                ans.append(str1[i-1])
+                ans+=str1[i-1]
                 i-=1
             else:
-                ans.append(str2[j-1])
+                ans+=str2[j-1]
                 j-=1
-        while j>0:
-            ans.append(str2[j-1])
-            j-=1
         while i>0:
-            ans.append(str1[i-1])
+            ans+=str1[i-1]
             i-=1
-        return ''.join(reversed(ans))
-
-
+        while j>0:
+            ans+=str2[j-1]
+            j-=1
+        return ans[::-1]
 
         
-            
-     
