c11c3d35dfc7c4566d86be2ca244b0088cfd536b
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-17 07:36:53 +0000

    Sync LeetCode submission - Distinct Subsequences (python)

--- problems/distinct_subsequences/solution.py
+++ problems/distinct_subsequences/solution.py
@@ -5,20 +5,18 @@ class Solution(object):
         :type t: str
         :rtype: int
         """
-        def subsequences(s,t):
-            n=len(s)
-            m=len(t)
-            dp=[[0]*(m+1) for _ in range(n+1)]
-            for i in range(n+1):
-                dp[i][0]=1
+        n=len(s)
+        m=len(t)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(m+1):
+            dp[0][i]=0
+        for i in range(n+1):
+            dp[i][0]=1
+        for i in range(1,n+1):
             for j in range(1,m+1):
-                dp[0][j]=0
-            for i in range(1,n+1):
-                for j in range(1,m+1):
-                    if s[i-1]==t[j-1]:
-                        dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
-                    else:
-                        dp[i][j]=dp[i-1][j]
-            return dp[n][m]
-        return subsequences(s,t)
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[n][m]
         
