14929214056b1418aff4d4f99be18ac41f654295
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-06-28 06:28:40 +0000

    Sync LeetCode submission - Maximum Element After Decreasing and Rearranging (python)

---
+++ problems/maximum_element_after_decreasing_and_rearranging/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        n=len(arr)
+        counts=[0]*(n+1)
+        for num in arr:
+            counts[min(num,n)]+=1
+        ans=1
+        for num in range(2,n+1):
+            ans=min(ans+counts[num],num)
+        return ans
+
+        
