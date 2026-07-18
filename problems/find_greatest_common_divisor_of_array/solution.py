65546e3ac076318954015946e453cc5618454eee
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-07-18 05:29:19 +0000

    Sync LeetCode submission - Find Greatest Common Divisor of Array (python)

---
+++ problems/find_greatest_common_divisor_of_array/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def gcd(a,b):
+            while b:
+                a,b=b,a%b
+            return a
+        return gcd(min(nums),max(nums))
+        
