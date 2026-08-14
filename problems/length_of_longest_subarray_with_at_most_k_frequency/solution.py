417e446cba839448e91effff7ceaf809be4fcc5d
Adnan2942 <97748498+romancobblepot@users.noreply.github.com>
2026-08-14 07:58:20 +0000

    Sync LeetCode submission - Length of Longest Subarray With at Most K Frequency (python)

--- problems/length_of_longest_subarray_with_at_most_k_frequency/solution.py
+++ problems/length_of_longest_subarray_with_at_most_k_frequency/solution.py
@@ -9,19 +9,12 @@ class Solution(object):
         r=0
         n=len(nums)
         freq=defaultdict(int)
-        cnt=defaultdict(int)
         max_len=0
-        max_freq=0
         while r<n:
-            cnt[freq[nums[r]]]-=1
             freq[nums[r]]+=1
-            cnt[freq[nums[r]]]+=1
-            max_freq=max(max_freq,freq[nums[r]])
-            if max_freq>k:
-                while cnt[max_freq]>0:
-                    cnt[freq[nums[l]]]-=1
-                    freq[nums[l]]-=1
-                    l+=1
+            while freq[nums[r]]>k:
+                freq[nums[l]]-=1
+                l+=1
             max_len=max(max_len,r-l+1)
             r+=1
         return max_len
